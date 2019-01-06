e210d856e0950b78c663c109bf2f57ae2561592e
dimchoSCR <dimchokarpachev@gmail.com>
2019-01-06 19:39:30 +0200

    Add get resource name from Uri function

---
+++ app/src/main/java/apps/dcoder/smartbellcontrol/util/UriUtil.kt
@@ -0,0 +1,26 @@
+package apps.dcoder.smartbellcontrol.util
+
+import android.net.Uri
+import android.util.Log
+import java.io.File
+import java.lang.IllegalStateException
+
+fun Uri.getFileName(): String {
+
+    val uri = this
+
+    // For example: <Storage Type>(primary/SD card):<Folder>/<Folder>/<File Name>
+    val uriLastPathSegment = uri.lastPathSegment
+    Log.d("DK", uriLastPathSegment)
+
+    val splitLastPathSegment: List<String> = uriLastPathSegment.split(":")
+    if (splitLastPathSegment.size <= 1 || splitLastPathSegment.size > 2) {
+        throw IllegalStateException("Too many parts in split uri segment!")
+    }
+
+    val filePath = splitLastPathSegment[1] // For example: <Folder>/<Folder>/<FileName>
+    val lastIndexOfDelimiter = filePath.lastIndexOf(File.separatorChar)
+
+    return filePath.substring(lastIndexOfDelimiter + 1)
+
+}
