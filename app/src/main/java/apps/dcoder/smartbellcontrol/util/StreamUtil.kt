3ac723bc63af9b73f35b3fbf72e1439bf8c2a34d
dimchoSCR <dimchokarpachev@gmail.com>
2019-01-06 19:35:12 +0200

    Made getBytes() async

--- app/src/main/java/apps/dcoder/smartbellcontrol/util/StreamUtil.kt
+++ app/src/main/java/apps/dcoder/smartbellcontrol/util/StreamUtil.kt
@@ -1,23 +1,23 @@
 package apps.dcoder.smartbellcontrol.util
 
 import android.util.Log
+import apps.dcoder.smartbellcontrol.AppExecutors
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
 
+fun InputStream.getBytesAsync(onGetBytesSuccess: (ByteArray) -> Unit) {
 
-
-fun InputStream.getBytes(): ByteArray {
     val inputStream = this
     val outputStream = ByteArrayOutputStream()
-//    val buffer: ByteArray = ByteArray(BUFFER_SIZE)
-//    var length = 0
-//    do{
-//        length = inputStream.read(buffer,0, buffer.size)
-//        outputStream.write(buffer,0, length)
-//    } while (length != -1)
 
-    val bytesCopied = inputStream.copyTo(outputStream)
-    Log.d("DK", "Bytes copied: $bytesCopied")
+    AppExecutors.singleExecutor.execute {
+        val bytesCopied = inputStream.copyTo(outputStream)
+        Log.d("DK", "Bytes copied: $bytesCopied")
 
-    return outputStream.toByteArray()
+        AppExecutors.MainThreadExecutor.execute {
+            onGetBytesSuccess(outputStream.toByteArray())
+            outputStream.close()
+        }
+
+    }
 }
