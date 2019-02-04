a63f42c92f4de014b698466329b72f130471224f
dimchoSCR <dimchokarpachev@gmail.com>
2019-02-04 20:09:12 +0200

    Add request for retrieving information for all melodies

--- app/src/main/java/apps/dcoder/smartbellcontrol/restapiclient/SmartBellAPI.kt
+++ app/src/main/java/apps/dcoder/smartbellcontrol/restapiclient/SmartBellAPI.kt
@@ -1,10 +1,9 @@
-package apps.dcoder.smartbellcontrol
+package apps.dcoder.smartbellcontrol.restapiclient
 
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.*
 
 interface SmartBellAPI {
