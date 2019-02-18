02be9bd4147432b9e87fbc4fade170b35d906625
dimchoSCR <dimchokarpachev@gmail.com>
2019-02-18 21:14:44 +0200

    Add endpoint for ring log retrieval

--- app/src/main/java/apps/dcoder/smartbellcontrol/restapiclient/SmartBellAPI.kt
+++ app/src/main/java/apps/dcoder/smartbellcontrol/restapiclient/SmartBellAPI.kt
@@ -1,6 +1,7 @@
 package apps.dcoder.smartbellcontrol.restapiclient
 
 import apps.dcoder.smartbellcontrol.restapiclient.model.RawMelodyInfo
+import apps.dcoder.smartbellcontrol.restapiclient.model.RawRingEntry
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import okhttp3.ResponseBody
@@ -8,6 +9,7 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface SmartBellAPI {
+
     @Multipart
     @POST("melodies/upload")
     fun uploadMelody(@Part file: MultipartBody.Part): Call<Void>
@@ -17,4 +19,8 @@ interface SmartBellAPI {
 
     @GET("melodies/")
     fun getAvailableMelodies(): Call<List<RawMelodyInfo>>
+
+    @GET("melodies/log")
+    fun getRingLogEntries(@Query("compSign") compSign: String,
+                          @Query("timeString") timeString: String): Call<List<RawRingEntry>>
 }
---
+++ app/src/main/java/apps/dcoder/smartbellcontrol/restapiclient/model/RawRingEntry.java
@@ -0,0 +1,33 @@
+package apps.dcoder.smartbellcontrol.restapiclient.model;
+
+public class RawRingEntry {
+
+    private long id;
+    private String melodyName;
+    private String dateTime;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMelodyName() {
+        return melodyName;
+    }
+
+    public void setMelodyName(String melodyName) {
+        this.melodyName = melodyName;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+}
